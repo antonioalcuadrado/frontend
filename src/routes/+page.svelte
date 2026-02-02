00a180cd8860508891c1eefefee887c4629b512b
aperezmarquez <aperezmarquez.business@gmail.com>
2026-02-03 00:15:29 +0100

    Added: url change from home button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
     import './sass/home.sass'
+import { goto } from '$app/navigation'
 </script>
 
 <div class="home">
     <section class="home-body">
         <h1 class="home-body-title">Personaliza tu horario</h1>
         <p class="home-body-txt">Usa nuestra herramienta para conseguir el mejor horario posible y deja de pasar 12 horas dentro de la universidad, para solo 4 horas de clases.</p>
-        <button class="home-body-create">Crea tu horario</button>
+        <button class="home-body-create" on:click={() => {goto("/horario")}}>Crea tu horario</button>
     </section>
 </div>
