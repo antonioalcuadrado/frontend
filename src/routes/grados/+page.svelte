45dbbfd4e8472c21478ff52e7d4ef46265ba8767
aperezmarquez <aperezmarquez.business@gmail.com>
2026-02-02 18:54:33 +0100

    Update: added sass to the group schedule searcher

--- src/routes/grados/+page.svelte
+++ src/routes/grados/+page.svelte
@@ -4,6 +4,8 @@
     import { Schedule } from '../../lib/features/schedule'
     import { onMount } from 'svelte';
 
+    let selected_semester = ''
+
     let groups = []
     let query = ''
     let results = []
@@ -23,9 +25,12 @@
     $: results = query.length > 0 ? groups.filter(group => group.code.toLowerCase().includes(query.toLowerCase())) : []
 
     const handleClick = async (item) => {
+        if (!selected_semester) alert("Selecciona un cuatrimestre")
         const id = item.id
+
+        classes = await fetchLecturesByGroup(id, parseInt(selected_semester.split(' ')[1]));
         
-        classes = await fetchLecturesByGroup(id, 2)
+        query = ''
     }
 </script>
 
@@ -35,24 +40,31 @@
     </div>
 {:else}
     <div class="degree">
-        <div class="degree-search">
-            <input
-                type="text"
-                placeholder="Buscar…"
-                bind:value={query}
-                autocomplete="off"
-            />
+        <section class="degree-search">
+            <div class="search-bar">
+                <input class="input"
+                    type="text"
+                    placeholder="Busca el alias de tu grado…"
+                    bind:value={query}
+                    autocomplete="off"
+                />
 
-            {#if results.length}
-                <div class="results">
-                    {#each results as item}
-                        <button on:click={() => handleClick(item)}>
-                            {item.code}
-                        </button>
-                    {/each}
-                </div>
-            {/if}
-        </div>
+                {#if results.length}
+                    <div class="results">
+                        {#each results as item}
+                            <button on:click={() => handleClick(item)}>
+                                {item.code}
+                            </button>
+                        {/each}
+                    </div>
+                {/if}
+            </div>
+            <select class="semester-picker" bind:value={selected_semester}>
+                <option class="option" value="" selected disabled>Selecciona un cuatrimestre</option>
+                <option class="option">Cuatrimestre 1</option>
+                <option class="option">Cuatrimestre 2</option>
+            </select>
+        </section>
         <div class="degree-schedule">
             <Schedule id="schedule" classes={classes} />
         </div>
