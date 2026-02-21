f478e22921dd8b9c4348cf0f7456adbc6876b58d
aperezmarquez <aperezmarquez.business@gmail.com>
2026-02-21 18:55:04 +0100

    Added: The hall selection remembers the previously selected building and floor

--- src/routes/aulas/+page.svelte
+++ src/routes/aulas/+page.svelte
@@ -4,11 +4,13 @@
     import { goto } from '$app/navigation'
     import { BigDropdown, HallCard } from '$lib/components'
     import { fetchAllHalls } from '../../hooks/halls.js'
+    import { buildingStore, openFloorsStore } from '$lib/store/building_store'
 
     let halls = []
     let floors = -1
-    let open = [false, false, false]
-
+    $: open = $openFloorsStore
+    $: selected = $buildingStore
+    
     const updateFloors = (code) => {
         const number_part = code.replace(/\D/g, "")
         const first_digit = parseInt(number_part[0], 10)
@@ -18,8 +20,12 @@
         }
     }
 
-    const handleChange = async (e) => {
-        const value = e.target.value
+    $: if (selected) {
+        handleChange(selected)
+        buildingStore.set(selected)
+    }
+
+    const handleChange = async (value) => {
         floors = -1
 
         const data = await fetchAllHalls()
@@ -29,6 +35,7 @@
 
     const handleDrop = (i) => {
         open[i] = !open[i]
+        openFloorsStore.set(open)
     }
 
     const getFloorHalls = (i) => {
@@ -45,8 +52,8 @@
 <div class="halls">
     <section class="halls-selector">
         <label>Edificio</label>
-        <select on:change={handleChange}>
-            <option value="0" disabled selected>Selecciona un edificio</option>
+        <select bind:value={selected}>
+            <option value="default" disabled selected>Selecciona un edificio</option>
             <option value="MD">Madrid</option>
             <option value="BE">Berlín</option>
             <option value="BR">Bruselas</option>
@@ -56,7 +63,7 @@
     <section class="halls-floors">
         {#if floors > -1}
             {#each Array(floors + 1) as _, i}
-                <BigDropdown onClick={handleDrop} name="Planta {i}" />
+                <BigDropdown onClick={handleDrop} name="Planta {i}" open={open[i]} />
                 {#if open[i]}
                     <div in:fade={{duration:300}} out:fade={{duration:200}}>
                         <div class="halls-floors-halls" in:slide={{duration: 300}} out:slide={{duration:200}}>
