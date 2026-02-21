f79a13550ad86303504a998cc9b475cd946026c6
aperezmarquez <aperezmarquez.business@gmail.com>
2026-02-21 20:08:25 +0100

    Added: You can save your custom schedule

--- src/routes/horario/personalizado/+page.svelte
+++ src/routes/horario/personalizado/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     import './sass/page.sass'
     import { horarioStore } from '$lib/store/horario_store'
+    import { customStore } from '$lib/store/custom_schedule_store'
     import { goto } from '$app/navigation'
     import { fetchSchedules } from '../../../hooks/schedules'
     import { Schedule } from '$lib/features/schedule'
-    import { Trash, RightArrow } from '$lib/icons'
+    import { Trash, Save } from '$lib/icons'
 
     let loading = true
     $: schedule = $horarioStore
@@ -23,16 +24,20 @@
     const handleVisualize = (id) => {
         goto(`/horario/personalizado/${id}`)
     }
+    
+    const handleSave = (sch) => {
+        customStore.set(sch)
+    }
 </script>
 
 <div class="custom-schedule-grid">
     {#if (!loading)}
         {#each schedule as sch, i}
             <div class="container">
-                <Schedule classes={sch} />
+                <Schedule classes={sch} onClick={handleVisualize} id={i} />
                 <div class="action-buttons">
-                    <button on:click={() => handleVisualize(i)}>
-                        Visualizar<RightArrow id="arrow" size=0.6 color="#fff" />
+                    <button on:click={() => handleSave(sch)}>
+                        Guardar en perfil<Save size=0.9 color="#fff" />
                     </button>
                     <button class="trash" on:click={() => handleDelete(i)}>
                         <Trash size=0.9 color="#fff"/>
--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,4 +1,22 @@
 <script>
+    import './sass/page.sass'
+    import { Schedule } from '$lib/features/schedule'
+    import { customStore } from '$lib/store/custom_schedule_store.js'
+    
+    $: schedule = $customStore
+    let loading = true
+
+    $: if (schedule) {
+        loading = false
+    }
 </script>
 
-<h1>USER</h1>
+<div class="saved-schedule">
+    {#if loading}
+        <p>Loading...</p>
+    {:else if schedule.length > 0}
+        <Schedule classes={schedule} />
+    {:else}
+        <p>No se ha guardado ningún horario</p>
+    {/if}
+</div>
