cdf3b83a2d993069e9d02830f35e158063b50397
aperezmarquez <aperezmarquez.business@gmail.com>
2026-02-21 11:40:52 +0100

    Added: Opening and closing for big dropdown in halls #14

--- src/lib/components/big_dropdown.svelte
+++ src/lib/components/big_dropdown.svelte
@@ -2,11 +2,13 @@
     import './sass/big_dropdown.sass'
     import { RightArrow } from '$lib/icons';
     export let name = "N/A";
+    export let onClick;
 
     let close = true;
 
     const handleClick = () => {
         close = !close;
+        onClick(parseInt(name[7]));
     }
 </script>
 
