5ffe9a481fdccf03b5a3164534d2d5a83c78766b
aperezmarquez <aperezmarquez.business@gmail.com>
2026-02-22 12:37:28 +0100

    Added: Now you can download the saved schedule

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -2,9 +2,12 @@
     import './sass/page.sass'
     import { Schedule } from '$lib/features/schedule'
     import { customStore } from '$lib/store/custom_schedule_store.js'
+    import { Download } from '$lib/icons'
+    import { downloadSchedule } from '../../utils'
     
     $: schedule = $customStore
     let loading = true
+    let pdfRef
 
     $: if (schedule) {
         loading = false
@@ -12,11 +15,23 @@
 </script>
 
 <div class="saved-schedule">
-    {#if loading}
-        <p>Loading...</p>
-    {:else if schedule.length > 0}
-        <Schedule classes={schedule} />
-    {:else}
-        <p>No se ha guardado ningún horario</p>
-    {/if}
+    <section class="saved-schedule-info">
+        <p>Horario personalizado</p>
+        {#if !loading}
+            <button class="download" on:click={() => downloadSchedule(pdfRef)}>
+                Descargar horario <Download size=0.9 color="#fff" />
+            </button>
+        {/if}
+    </section>
+    <section class="saved-schedule-viewer">
+        {#if loading}
+            <p>Loading...</p>
+        {:else if schedule.length > 0}
+            <div bind:this={pdfRef}>
+                <Schedule classes={schedule} />
+            </div>
+        {:else}
+            <p>No se ha guardado ningún horario</p>
+        {/if}
+    </section>
 </div>
